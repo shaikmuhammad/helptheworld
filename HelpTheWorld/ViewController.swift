59956d29eba12adec8eb924d04b2ec3050c8d59e
Shaik Muhammad Abdul Razak <shaikm@Shaiks-MacBook-Pro.local>
2017-01-10 15:35:49 +0400

    Initial Commit

---
+++ HelpTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelpTheWorld
+//
+//  Created by Shaik Muhammad Abdul Razak on 2017-01-10.
+//  Copyright © 2017 Shaik Muhammad Abdul Razak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
